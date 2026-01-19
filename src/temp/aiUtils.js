184ef09afad7ef35b62e3e3229fc52026684fbf4
coinvest518 <coinvest518@gmail.com>
2026-01-19 15:55:59 -0500

    aiutils

---
+++ src/temp/aiUtils.js
@@ -0,0 +1,3 @@
+// Proxy file for deployment environments that place sources under /src
+// This delegates to the repository's top-level temp/aiUtils implementation.
+module.exports = require('../../temp/aiUtils');
